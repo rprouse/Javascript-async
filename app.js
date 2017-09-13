595b86aa950fc041bb43e6072c413f98f6c61163
Rob Prouse <rob@prouse.org>
2017-09-13 17:07:34 -0400

    Second demo attempt

--- app.js
+++ app.js
@@ -21,28 +21,24 @@ const options = {
   }
 };
 
-function getIssues() {
-  return request('https://api.github.com/issues', options);
-}
+let getIssues = async () =>
+  await request('https://api.github.com/issues', options);
 
-function getComments(issue) {
-    return request(issue.comments_url, options);
-}
+let getComments = async (issue) =>
+    await request(issue.comments_url, options);
 
-function displayIssuesWithComments() {
-  getIssues().then(function(data){
+async function displayIssuesWithComments() {
+  try {
+    let data = await getIssues();
     let issues = JSON.parse(data);
     for (let issue of issues) {
-        getComments(issue).then(function(data) {
-          let comments = JSON.parse(data);
-          console.log(issue.repository.full_name + '#' + issue.number + ' - ' + issue.title + ' (' + comments.length + ' comments)');
-      }).catch(function(error) {
-          console.error(error);
-      });
+      let data = await getComments(issue);
+      let comments = JSON.parse(data);
+      console.log(issue.repository.full_name + '#' + issue.number + ' - ' + issue.title + ' (' + comments.length + ' comments)');
     }
-  }).catch(function(error) {
+  } catch(error) {
     console.error(error);
-  });
+  }
 }
 
 displayIssuesWithComments();
